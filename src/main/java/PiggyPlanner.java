7de931a34f2dfd5817d96d24a4bdd776a486f700
teesha902 <atreyee.basak@u.nus.edu>
2025-01-22 11:31:24 +0800

    Completed Level 0: Changed bot name, added greeting and goodbye

---
+++ src/main/java/PiggyPlanner.java
@@ -0,0 +1,20 @@
+public class PiggyPlanner {
+    public static void main(String[] args) {
+        printHorizontalLine();
+        System.out.println(" Hello! I'm you PiggyPlanner");
+        System.out.println(" What shall we do today?");
+        printHorizontalLine();
+
+        // Exit message
+        System.out.println(" Bye. Hope to see you again soon!");
+    }
+
+    private static void printHorizontalLine() {
+        System.out.println("____________________________________________________________");
+    }
+}
+
+
+
+
+
