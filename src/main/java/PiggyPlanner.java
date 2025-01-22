dc2529113ab220970008676d749e56ef7ba9d30d
teesha902 <atreyee.basak@u.nus.edu>
2025-01-22 12:40:43 +0800

    Level 1 completed: Added echo and 'bye' command functionality

--- src/main/java/PiggyPlanner.java
+++ src/main/java/PiggyPlanner.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class PiggyPlanner {
     public static void main(String[] args) {
         printHorizontalLine();
@@ -5,8 +7,21 @@ public class PiggyPlanner {
         System.out.println(" What shall we do today?");
         printHorizontalLine();
 
+        Scanner reader = new Scanner(System.in);
+        while (true) {
+            String userInput = reader.nextLine();
+            if (userInput.equals("bye")) {
+                break;
+            }
+            printHorizontalLine();
+            System.out.println(userInput);
+            printHorizontalLine();
+        }
+
         // Exit message
+        printHorizontalLine();
         System.out.println(" Bye. Hope to see you again soon!");
+        printHorizontalLine();
     }
 
     private static void printHorizontalLine() {
