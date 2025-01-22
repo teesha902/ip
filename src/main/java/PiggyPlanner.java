0c721d3bea647a461185cb7e6aa9f337026b9f27
teesha902 <atreyee.basak@u.nus.edu>
2025-01-22 13:07:17 +0800

    Level 2 completed: Made taskList to store all tasks, able add user input into taskList

--- src/main/java/PiggyPlanner.java
+++ src/main/java/PiggyPlanner.java
@@ -1,30 +1,41 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class PiggyPlanner {
     public static void main(String[] args) {
-        printHorizontalLine();
-        System.out.println(" Hello! I'm you PiggyPlanner");
-        System.out.println(" What shall we do today?");
-        printHorizontalLine();
+        printToScreen(" Hello! I'm you PiggyPlanner \n What shall we do today?");
 
+        ArrayList<String> taskList = new ArrayList<>();
         Scanner reader = new Scanner(System.in);
         while (true) {
             String userInput = reader.nextLine();
+
             if (userInput.equals("bye")) {
                 break;
+            } else if (userInput.equals("list")) {
+                if (taskList.isEmpty()) {
+                    printToScreen("No tasks yet!");
+                } else {
+                    StringBuilder listOutput = new StringBuilder();
+                    listOutput.append("To-do list: \n");
+                    for (int i = 0; i < taskList.size(); i++) {
+                        listOutput.append((i + 1)).append(". ").append(taskList.get(i)).append("\n");
+                    }
+                    printToScreen(listOutput.toString().trim()); // Print all tasks at once
+                }
+            } else {
+                taskList.add(userInput);
+                printToScreen(" Added to list: " + userInput);
             }
-            printHorizontalLine();
-            System.out.println(userInput);
-            printHorizontalLine();
         }
 
         // Exit message
-        printHorizontalLine();
-        System.out.println(" Bye. Hope to see you again soon!");
-        printHorizontalLine();
+        printToScreen(" Bye. Hope to see you again soon!");
     }
 
-    private static void printHorizontalLine() {
+    private static void printToScreen(String txt) {
+        System.out.println("____________________________________________________________");
+        System.out.println(txt);
         System.out.println("____________________________________________________________");
     }
 }
