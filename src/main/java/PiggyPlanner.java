6f7a4378d7e8b91c75b365b952fb5711db54e38f
teesha902 <atreyee.basak@u.nus.edu>
2025-01-22 19:14:47 +0800

    Level 4 completed: Task class and subclasses made, main program logic adjusted to accomodate

--- src/main/java/PiggyPlanner.java
+++ src/main/java/PiggyPlanner.java
@@ -20,25 +20,48 @@ public class PiggyPlanner {
                     listOutput.append("To-do list: \n");
                     for (int i = 0; i < taskList.size(); i++) {
                         listOutput.append((i + 1))
-                                .append(". [").append(taskList.get(i).status()).append("] ")
                                 .append(taskList.get(i)).append("\n");
+                                //.append(". [").append(taskList.get(i).status()).append("] ")
+                                //.append(taskList.get(i)).append("\n");
                     }
                     printToScreen(listOutput.toString().trim()); // Print all tasks at once
                 }
+
             } else if (userInput.startsWith("mark")) {
                 int taskIndex = Integer.parseInt(userInput.split(" ")[1]) - 1; // Extract task index
                 taskList.get(taskIndex).mark();
-                printToScreen(" Good work! Let's continue working." +
+                printToScreen(" Good work! Let's keep going." +
                         "\n I've marked this task as done: \n  [X] " + taskList.get(taskIndex));
             } else if (userInput.startsWith("unmark")) {
                 int taskIndex = Integer.parseInt(userInput.split(" ")[1]) - 1; // Extract task index
                 taskList.get(taskIndex).unmark();
                 printToScreen(" Oops, no problem." +
                         "\n I've unmarked the task: \n  [ ] " + taskList.get(taskIndex));
-            } else {
-                Task newTask = new Task(userInput);
+
+            } else if (userInput.startsWith("todo")) {
+                String taskAction = userInput.substring(5).trim();
+                Task newTask = new ToDo(taskAction);
                 taskList.add(newTask);
-                printToScreen(" Added to list: " + userInput);
+                printTaskAdded(newTask, taskList.size());
+            } else if (userInput.startsWith("deadline")) {
+                int dueDateIndex = userInput.indexOf("/by");
+                String taskAction = userInput.substring(9, dueDateIndex).trim();
+                String dueDate = userInput.substring(dueDateIndex + 3).trim(); // +3 to skip "/by"
+                Task newTask = new Deadline(taskAction, dueDate);
+                taskList.add(newTask);
+                printTaskAdded(newTask, taskList.size());
+            } else if (userInput.startsWith("event")) {
+                int fromIndex = userInput.indexOf("/from");
+                int toIndex = userInput.indexOf("/to");
+                String taskAction = userInput.substring(6, fromIndex).trim();
+                String from = userInput.substring(fromIndex + 5, toIndex).trim(); // +5 to skip "/from"
+                String to = userInput.substring(toIndex + 3).trim(); // +3 to skip "/to"
+                Task newTask = new Event(taskAction, from, to);
+                taskList.add(newTask);
+                printTaskAdded(newTask, taskList.size());
+
+            } else {
+                printToScreen("I'm sorry, I don't understand that command :(");
             }
         }
 
@@ -50,6 +73,12 @@ public class PiggyPlanner {
         System.out.println("____________________________________________________________");
         System.out.println(txt);
         System.out.println("____________________________________________________________");
+    }
+
+    private static void printTaskAdded(Task task, int totalTasks) {
+        printToScreen("New task! I've added it to the list :) \n  " + task +
+                "\nNow we have a total of " + totalTasks + " things to do.");
+        //need to adjust print for when task = 1 or 0
     }
 }
 
