a28e8ef1ae5e8e5c1e1aa35ddce8951b00138ec9
teesha902 <atreyee.basak@u.nus.edu>
2025-01-28 20:51:14 +0800

    pdate Event.java to use java.time.LocalDateTime instead of storing time information in strings

--- src/main/java/tasks/Event.java
+++ src/main/java/tasks/Event.java
@@ -1,33 +1,41 @@
 package tasks;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+//example: deadline return book /from 2/12/2019 1800 /to 2/12/2019 1800
 public class Event extends Task{
     // tasks that start at a specific date/time and ends at a specific date/time
-    protected String  start;
-    protected String end;
+    //protected String  start;
+    //protected String end;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private static final DateTimeFormatter INPUT_FORMATTER = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
+    private static final DateTimeFormatter OUTPUT_FORMATTER = DateTimeFormatter.ofPattern("EEEE, MMM dd yyyy, h:mma");
 
     public Event(String name, String start, String end) {
         super(name);
-        this.start = start;
-        this.end = end;
+        this.start = LocalDateTime.parse(start, INPUT_FORMATTER);
+        this.end = LocalDateTime.parse(end, INPUT_FORMATTER);
     }
 
-    public Event(String name, String start, String end, boolean isDone) {
+    public Event(String name, LocalDateTime start, LocalDateTime end, boolean isDone) {
         super(name);
         this.start = start;
         this.end = end;
         this.isDone = isDone;
     }
 
-    public String getStart() {
+    public LocalDateTime getStart() {
         return start;
     }
 
-    public String getEnd() {
+    public LocalDateTime getEnd() {
         return end;
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + start + " to: " + end + ")";
-    }
+        return "[E]" + super.toString() + " (from: " + start.format(OUTPUT_FORMATTER).toLowerCase() +
+                " to: " + end.format(OUTPUT_FORMATTER).toLowerCase() + ")";
 }
