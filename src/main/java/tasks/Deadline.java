8bf5a9a3e2ee1ce8ce4ad6d4fc53b72dfa4259d5
teesha902 <atreyee.basak@u.nus.edu>
2025-01-28 20:46:24 +0800

    Update Deadline.java to use java.time.LocalDateTime instead of storing time information in strings

--- src/main/java/tasks/Deadline.java
+++ src/main/java/tasks/Deadline.java
@@ -1,26 +1,35 @@
 package tasks;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+//example: deadline return book /by 2/12/2019 1800
 public class Deadline extends Task{
     //tasks that need to be done before a specific date/time
-    protected String due;
+    //protected String due;
+    private LocalDateTime dueDate;
+    private static final DateTimeFormatter INPUT_FORMATTER = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
+    private static final DateTimeFormatter OUTPUT_FORMATTER = DateTimeFormatter.ofPattern("EEEE, MMM dd yyyy, h:mma");
+    //EEEE → Full day name and h:mma → 12-hour format with AM/PM
 
-    public Deadline(String name, String due) {
+    public Deadline(String name, String dueDate) {
         super(name);
-        this.due = due;
+        this.dueDate = LocalDateTime.parse(dueDate, INPUT_FORMATTER); // Convert input string to LocalDateTime
     }
 
-    public Deadline(String name, String due, boolean isDone) {
+    public Deadline(String name, LocalDateTime dueDate, boolean isDone) {
         super(name);
-        this.due = due;
+        //this.due = due;
+        this.dueDate = dueDate;
         this.isDone = isDone;
     }
 
-    public String getDue() {
-        return due;
+    public LocalDateTime getDueDate() {
+        return dueDate;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + due + ")";
+        return "[D]" + super.toString() + " (by: " + dueDate.format(OUTPUT_FORMATTER).toLowerCase() + ")";
     }
 }
