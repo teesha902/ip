0501854de6447fa67ab4a2cc5b0c1e7d5913eeab
teesha902 <atreyee.basak@u.nus.edu>
2025-01-30 00:59:15 +0800

    Complete A-OOP: Made program use more OOP by using Ui class to handle messages to print to screen and a TaskList class to have a TaskList object. Rest of the program already OOP based.

---
+++ src/main/java/tasks/TaskList.java
@@ -0,0 +1,20 @@
+package tasks;
+
+import java.util.ArrayList;
+
+public class TaskList {
+    private final ArrayList<Task> tasks;
+
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public TaskList(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public ArrayList<Task> getAllTasks() {
+        return tasks;
+    }
+
+}
